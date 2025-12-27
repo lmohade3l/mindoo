33986154148f74b0279952572a84004fe5899b37
mohadese.gh <m.ghasemmehrabi@tosansoha.com>
2025-12-27 16:12:51 +0330

    feat: add page loading component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { PageLoading } from "@/components/PageLoading";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-[black]">this is the mindoo game app</div>
+    <PageLoading backgroundColor="#FBD302">
+      <div className="text-[black]">this is the mindoo game app</div>
+
+    </PageLoading>
   );
 }
---
+++ components/PageLoading.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+interface PageLoadingProps {
+    children: React.ReactNode;
+    duration?: number;
+    backgroundColor?: string;
+}
+
+export const PageLoading = ({
+    children,
+    duration = 800,
+    backgroundColor
+}: PageLoadingProps) => {
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, duration);
+        return () => clearTimeout(timer);
+    }, [duration]);
+
+    return (
+        <>
+            {loading ? (
+                <div style={{ backgroundColor: backgroundColor ?? 'red' }} className={`min-h-screen flex items-center justify-center`}>
+                    <div className="flex flex-col gap-3">
+                        <div>
+                            <p className="text-[black] text-[32px]">مایندو</p>
+                        </div>
+                    </div>
+                </div>
+            ) :
+                children
+            }
+        </>
+    );
+}
